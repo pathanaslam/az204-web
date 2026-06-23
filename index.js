06e9fb519cafab054274c2ff69a1507a071b901b
pathanaslam <aslamkhan9059977259@gmail.com>
2026-06-23 11:33:31 +0530

    Second Commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-res.send(`Hello from AZ-204!`);
+res.send(`Hello from AZ-205!`);
 });
 app.get('/health', (req, res) => res.json({ status: 'ok', time: new Date().toISOString() }));
 app.listen(port, () => console.log(`Listening on ${port}`));
