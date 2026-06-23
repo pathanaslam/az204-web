8a4cb98b28d6df0a7d1fa54f7ae696b622e35916
pathanaslam <aslamkhan9059977259@gmail.com>
2026-06-23 10:16:01 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+res.send(`Hello from AZ-204!`);
+});
+app.get('/health', (req, res) => res.json({ status: 'ok', time: new Date().toISOString() }));
+app.listen(port, () => console.log(`Listening on ${port}`));
